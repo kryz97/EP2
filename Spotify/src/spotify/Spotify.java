e1ea35cc83d5289edfaa532c48284438235130dd
kryz97 <cbarquero40796@ufide.ac.cr>
2023-06-28 18:30:28 -0600

    Subir Chris

---
+++ Spotify/src/spotify/Spotify.java
@@ -0,0 +1,18 @@
+
+package spotify;
+
+
+public class Spotify {
+
+    public static void main(String[] args) {
+        playList pl = new playList();
+        
+        pl.id();
+        //Persona p1 = new Persona();
+        
+        
+    }
+
+    
+}
+
---
+++ Spotify/src/spotify/playList.java
@@ -0,0 +1,43 @@
+
+package spotify;
+
+public class playList {
+    public int id = 0;
+    public String author = "";
+    public float time = 0;
+    public int year = 0;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public float getTime() {
+        return time;
+    }
+
+    public void setTime(float time) {
+        this.time = time;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+    
+    
+}
